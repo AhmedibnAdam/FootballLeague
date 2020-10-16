1e61f5d2fc6cbb17b1dd8b5003524e04e2d68daa
Ahmed_Adam <ibn.abuadam@gmail.com>
2020-10-16 02:16:14 +0200

    Realm

--- FootballLeague/Shared/Realm/RealmExtension.swift
+++ FootballLeague/Shared/Realm/RealmExtension.swift
@@ -6,24 +6,24 @@
 //  Copyright (c) 2020 TaskApp. All rights reserved.
 //
 
-//import Foundation
-//import  RealmSwift
-//
-//
-//extension Object {
-//    func safeWrite (operation: ()->()){
-//        if let realm = realm {
-//            do {
-//                try realm.write {
-//                    operation()
-//                }
-//            }
-//            catch {
-//                print(error)
-//            }
-//        }
-//        else {
-//            operation()
-//        }
-//    }
-//}
+import Foundation
+import  RealmSwift
+
+
+extension Object {
+    func safeWrite (operation: ()->()){
+        if let realm = realm {
+            do {
+                try realm.write {
+                    operation()
+                }
+            }
+            catch {
+                print(error)
+            }
+        }
+        else {
+            operation()
+        }
+    }
+}
